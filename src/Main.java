5c953fcffb1ebdfbf9ba4438416359cc5de18658
Salvatore Catania <salvatore.catania@live.it>
2019-10-28 14:12:28 +0100

    Initial commit...

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+
+public class Main {
+	
+	//Divide
+	public void mergeSort(int ar[], int n) {
+		//1° Step
+		//In base ai principi del Merge Sort se la lunghezza del vettore è >= 1 l'array è già ordinato
+		if(n < 2)
+			return;
+		
+		//2° Step
+		//Identificati attori dell'algoritmo
+		//Mid è la lunghezza media del vettore
+		//l[] è il vettore di sinistra che dovrà essere scomposto a sua volta
+		//r[] è il vettore di destra che dovrà essere scomposto a sua volta
+		int mid = n / 2;
+		int l[] = new int[mid];
+		int r[] = new int[n - mid];
+		
+		//Passo i valori all'interno di l
+		for(int i = 0; i < mid; i++) 
+			l[i] = ar[i];
+		
+		//Passo i valori all'interno di r
+		for(int i = mid; i < n; i++) 
+			r[i - mid] = ar[i];
+		
+		//3° Step
+		//Ordino i due sub-arrays
+		mergeSort(l, mid);
+		mergeSort(r, n - mid);
+		
+		//4° step
+		//Eseguo il merge dei due sub-arrays
+		merge(ar, l, r, mid, n - mid);
+	}
+	
+	//Impera
+	public static void merge(int ar[], int l[], int r[], int left, int right) {
+		int i = 0, k = 0, j = 0;
+		
+		while(i < left && j < right) {
+			if(l[i] <= r[j])
+				ar[k++] = l[i++];
+			else
+				ar[k++] = r[j++];
+		}
+		
+		while(i < left) {
+			ar[k++] = l[i++];
+		}
+		while(j < right) {
+			ar[k++] = r[j++];
+		}
+	}
+	
+	public static void main(String []args) {
+		System.out.println("Merge Sort");
+		int ar[] = {3,6,19,4,5,0,8,2};
+		
+		Main main = new Main();
+		main.mergeSort(ar, ar.length);
+		
+		System.out.println(Arrays.toString(ar));
+	}
+	
+}
